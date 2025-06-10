d5be747effbca52d84e5f195f7546a4caafd9ce1
Lskar <3074524288@qq.com>
2025-06-10 22:43:07 +0800

    BookMapper->update方法

--- src/main/java/org/lskar/librarysystem/mapper/BookMapper.java
+++ src/main/java/org/lskar/librarysystem/mapper/BookMapper.java
@@ -1,7 +1,12 @@
 package org.lskar.librarysystem.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.lskar.librarysystem.entity.Book;
 
 @Mapper
 public interface BookMapper {
+
+    void updateById(Book book);
+
+
 }
